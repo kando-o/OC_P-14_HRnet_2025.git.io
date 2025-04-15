9251d73c79deebb4e7776f74de68970739e78e46
kando-o <64841289+kando-o@users.noreply.github.com>
2025-04-15 19:51:57 +0200

    Master - Add Header

---
+++ src/Component/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavLink } from 'react-router'
+
+export default function Header() {
+	return (
+		<div className='header'>
+			<h1>HRnet</h1>
+			<h1>Current Employees</h1>
+		</div>
+	)
+}
